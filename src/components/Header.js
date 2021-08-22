9d7939177958355ff1f2ec617cbec9aca758e076
allilk <milk@cream.glass>
2021-08-22 08:50:22 -0400

    navigation update

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,23 @@
 import React, { Component } from "react";
+import { Link } from "react-router-dom";
 
 export class Header extends Component {
-    render() {
-        return(
-            <div className="bg-gray-400 text-white p-4 mb-3">
-                cream.glass
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div>
+        <div
+          style={{ backgroundColor: "#b2d5eb" }}
+          className="text-white p-4"
+        >
+          <div className="inline absolute left-0 top-0 py-4 mx-4">
+            <Link to="">cream.glass</Link>
+          </div>
+          <div className="inline absolute right-0 top-0 text-2xl py-2 mx-4">
+              <Link to="/create">+</Link>
+          </div>
+          <br/>
+        </div>
+      </div>
+    );
+  }
 }
