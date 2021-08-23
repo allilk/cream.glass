80632acd91d7ed204a256bee2d2dfa550ab2662b
allilk <milk@cream.glass>
2021-08-23 09:51:04 -0400

    more creation page work

--- src/components/Creator.js
+++ src/components/Creator.js
@@ -1,17 +1,39 @@
-import React, { useRef } from "react";
+import React, { useRef, useState } from "react";
 
 export const Creator = () => {
-	let ingredientVal = useRef();
-	let ingredientArr = [""];
-	const getVal = (e) => {
-		console.log(e.target.value);
+	let ingredientVal;
+	const initialValue = [];
+	const [ingredients, setIngredients] = useState(initialValue);
+	const onChangeVal = (e) => {
+		ingredientVal = e.target.value;
 	};
+	const handleAdd = (e) => {
+		e.preventDefault();
+		if (ingredientVal) {
+			setIngredients([...ingredients, ingredientVal]);
+		}
+	};
+	const items = ingredients.map((item, i) => {
+		return (
+			<div className="grid grid-cols-12 mt-2">
+				<input
+					className="col-span-1 border-2 border-black md:px-4 bg-black hover:bg-gray-700 text-white md:text-center items-baseline text-lg"
+					type="submit"
+					onClick={handleAdd}
+					value="x"
+				/>
+				<div className="border-2 p-2 border-black col-start-2 col-span-11">
+					<li key={i}>{item}</li>
+				</div>
+			</div>
+		);
+	});
 	return (
 		<>
 			<div>
 				<center>
 					<br />
-					<div className="border-2 border-blue-300 p-4 mx-4 md:mx-32 ">
+					<div className="border-2 border-b-0 border-blue-300 p-4 mx-4 md:mx-32 h-auto ">
 						<div className="my-4 text-4xl mb-6 tracking-widest text-center">
 							CREATE NEW RECIPE
 						</div>
@@ -40,38 +62,42 @@ export const Creator = () => {
 									></textarea>
 								</div>
 							</div>
-							<div className="text-left text-sm row-span-1 col-span-full md:row-start-5 md:col-span-auto mt-10 md:mt-0">
+							<div className="text-left text-sm col-span-full md:row-start-5 md:col-span-auto mt-10 md:mt-0">
 								<div className="mb-1">Ingredients</div>
 								<div className="grid grid-cols-12">
-									<input
-										className="col-span-1 border-2 border-black md:px-4 bg-black hover:bg-gray-700 text-white md:text-center items-baseline text-xl"
-										type="submit"
-										onClick={() => {
-											ingredientArr = [
-												...ingredientArr,
-												ingredientArr,
-											];
-											console.log(ingredientVal);
-										}}
-										value="+"
-									/>
-									<div className="col-start-2 col-span-11">
+									<form className="contents">
 										<input
-											className="border-2 p-2 border-black w-full"
-											type="text"
-											ref={getVal()}
-										></input>
-									</div>
+											className="col-span-1 border-2 border-black md:px-4 bg-black hover:bg-gray-700 text-white md:text-center items-baseline text-xl"
+											type="submit"
+											onClick={handleAdd}
+											value="+"
+										/>
+										<div className="col-start-2 col-span-11">
+											<input
+												className="border-2 p-2 border-black w-full"
+												onChange={onChangeVal}
+												type="text"
+												required
+											></input>
+										</div>
+									</form>
+								</div>
+								<div className="mt-2 overscroll-auto">
+									{items}
+								</div>
+								<br />
+
+								<div>
+									<center>
+										<input
+											type="submit"
+											className=" bottom-0 rounded-full font-semibold px-32 py-2 bg-blue-300 hover:bg-blue-500 "
+											value="CREATE"
+										/>
+									</center>
 								</div>
 							</div>
-
-							<br />
 						</div>
-						<input
-							type="submit"
-							className=" bottom-0 rounded-full font-semibold px-32 py-2 bg-blue-300 hover:bg-blue-500 "
-							value="CREATE"
-						/>
 					</div>
 				</center>
 			</div>
