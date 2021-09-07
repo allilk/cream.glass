c2031b061919ff8df74edaf5bccc8d881307246d
allilk <milk@cream.glass>
2021-09-07 10:19:09 -0400

    Registration page redesign

--- src/components/Register.js
+++ src/components/Register.js
@@ -32,7 +32,9 @@ export const Register = (props) => {
 		const password = e.target.value;
 		setPassword(password);
 	};
-
+	const doesPasswordMatch = (e) => {
+		const repassword = e.target.value;
+	};
 	const handleRegister = (e) => {
 		e.preventDefault();
 		setSuccessful(false);
@@ -53,58 +55,92 @@ export const Register = (props) => {
 		<div>
 			<Message />
 			<center>
-				<div className="border-2 border-blue-300 w-5/6 md:w-1/4 h-auto bg-white rounded-md">
+				<div className="border-2 border-blue-300 mx-6 md:mx-0 md:w-1/4 h-auto bg-white rounded-md">
 					<br />
 					<div className="text-4xl mb-6 tracking-widest">
 						REGISTER
 					</div>
 					<div className="mb-12">
-						<form onSubmit={handleRegister}>
-							<input
-								className="border-2 border-blue-300 rounded px-4 py-1"
-								{...register("name", {
-									required: true,
-									maxLength: 48,
-								})}
-								placeholder="Name"
-								onChange={onChangeName}
-								type="text"
-								maxLength="48"
-								required
-							/>
-							<br />
-							<br />
-							<input
-								className="border-2 border-blue-300 rounded px-4 py-1"
-								{...register("email", {
-									required: true,
-									maxLength: 64,
-								})}
-								placeholder="Email"
-								onChange={onChangeEmail}
-								type="email"
-								maxLength="64"
-								required
-							/>
-							<br />
-							<br />
-							<input
-								className="border-2 border-blue-300 rounded px-4 py-1"
-								{...register("password", {
-									required: true,
-									maxLength: 16,
-								})}
-								placeholder="Password"
-								onChange={onChangePassword}
-								type="password"
-								maxLength="16"
-								required
-							/>
-							<br />
+						<form className="contents" onSubmit={handleRegister}>
+							<div className="mb-2">
+								<div className="ml-12 text-left pb-1 text-sm">
+									Name
+								</div>
+								<div>
+									<input
+										className="border-2 border-blue-300 rounded px-4 py-1"
+										{...register("name", {
+											required: true,
+											maxLength: 48,
+										})}
+										onChange={onChangeName}
+										type="text"
+										maxLength="48"
+										required
+									/>
+								</div>
+								<div className="text-xs underline text-gray-500 mb-2">
+									This will be used when you create recipes!
+								</div>
+							</div>
+							<div className="mb-2">
+								<div className="ml-12 text-left pb-1 text-sm">
+									Email
+								</div>
+								<div>
+									<input
+										className="border-2 border-blue-300 rounded px-4 py-1"
+										{...register("email", {
+											required: true,
+											maxLength: 64,
+										})}
+										onChange={onChangeEmail}
+										type="email"
+										maxLength="64"
+										required
+									/>
+								</div>
+							</div>
+							<div className="mb-2">
+								<div className="ml-12 text-left pb-1 text-sm">
+									Password
+								</div>
+								<div>
+									<input
+										className="border-2 border-blue-300 rounded px-4 py-1"
+										{...register("password", {
+											required: true,
+											maxLength: 16,
+										})}
+										onChange={onChangePassword}
+										type="password"
+										maxLength="16"
+										required
+									/>
+								</div>
+							</div>
+							<div className="mb-2">
+								<div className="ml-12 text-left pb-1 text-sm">
+									Re-enter Password
+								</div>
+								<div>
+									<input
+										className="border-2 border-blue-300 rounded px-4 py-1"
+										{...register("password", {
+											required: true,
+											maxLength: 16,
+										})}
+										onChange={onChangePassword}
+										type="password"
+										maxLength="16"
+										disabled
+									/>
+								</div>
+							</div>
 							<br />
 							<input
 								type="submit"
-								className="rounded-full font-semibold px-8 py-2 bg-blue-300 hover:bg-blue-500"
+								className=" font-semibold w-full py-4 bg-blue-300 -mb-12 hover:bg-blue-500 "
 								ref={checkBtn}
 								value="Register"
 							/>
